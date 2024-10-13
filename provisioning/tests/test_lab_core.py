84855c08b3dd9b77a0c407bdafd706cc44667029
Laszlo Racz <lracz@newpush.com>
2024-10-13 08:35:12 -0600

    🟢 provisioning/tests/test_lab_core.py

---
+++ provisioning/tests/test_lab_core.py
@@ -0,0 +1,57 @@
+import pytest
+
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwd")
+    assert passwd.contains("root")
+    assert passwd.user == "root"
+    assert passwd.group == "root"
+    assert passwd.mode == 0o644
+
+
+def test_docker_running_and_enabled(host):
+    docker = host.service("docker")
+    assert docker.is_running
+    assert docker.is_enabled
+
+def test_traefik_running_and_enabled(host):
+    traefik = host.docker("traefik")
+    assert traefik.is_running
+
+
+@pytest.mark.parametrize("service_name", [
+    "promtail",
+    "sshwifty",
+    "loki",
+    "traefik-forward-auth",
+    "traefik",
+    "mafl",
+    "casdoor",
+    "prometheus",
+    "grafana",
+    "node-exporter",
+    "watchtower",
+    "portainer",
+    "mafl-service-discovery",
+    "dockge",
+    "dozzle",
+    "cadvisor",
+])
+def test_core_docker_running_and_enabled(host, service_name):
+    service = host.docker(service_name)
+    assert service.is_running
+
+@pytest.fixture(scope="module")
+def ip_address(host):
+    return host.run("curl -4 https://ifconfig.me").stdout.strip()
+
+def test_auth_url_loads(host, ip_address):
+    url = f"https://auth.{ip_address}.traefik.me"
+    print(f"Testing URL: {url}")
+    response = host.run(f"curl -k -s -o /dev/null -w '%{{http_code}}' {url}")
+    assert response.stdout == "200"
+
+def test_www_url_loads(host, ip_address):
+    url = f"https://www.{ip_address}.traefik.me"
+    print(f"Testing URL: {url}")
+    response = host.run(f"curl -k -s -o /dev/null -w '%{{http_code}}' {url}")
+    assert response.stdout == "307" # Redirects to auth
